ba02f0f5c3ec7bcbad7b3cee39b8a0c933e8ab48
shaghayegh <shaghayeghmoasamee1391@gmail.com>
2024-11-07 11:42:34 +0330

    second version

--- sketch_nov7a/sketch_nov7a.ino
+++ sketch_nov7a/sketch_nov7a.ino
@@ -1,13 +1,19 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+int k;
 void setup() {
+  Serial.begin(9600);
 }
 
 void loop() {
-  lcd.init();
-  lcd.backlight();
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("hello");
+  if (Serial.available()) {
+    k = Serial.parseInt();
+    lcd.init();
+    lcd.backlight();
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print(k);
+  }
 }
